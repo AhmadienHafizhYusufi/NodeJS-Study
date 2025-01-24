0579a695f1fbd5ee6dd077c2b4a45fbc9956c10d
AHMADIEN HAFIZH YUSUFI <ahmadienhafizhyusufi@gmail.com>
2025-01-24 10:44:26 +0700

    Using NPM Packages
    Practicing use npm packages such as validator, chalk, and nodemon.

--- 8-npm/app.js
+++ 8-npm/app.js
@@ -1,1 +1,11 @@
-console.log("Hello World");
+// const validator = require('validator');
+
+// console.log(validator.isEmail('yowamomo@gmail.com'));
+// console.log(validator.isMobilePhone('08129840975', 'id-ID'));
+// console.log(validator.isNumeric('08129840975'));
+
+import chalk from "chalk";
+
+console.log(chalk.white.bgRed('Hello world!'));
+const pesan = chalk`lorem ipsum dolor sit amet, consectetur adipiscing elit.`;
+console.log(chalk.bgRed(pesan));
